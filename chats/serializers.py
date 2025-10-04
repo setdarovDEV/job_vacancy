3488b32c0c3da4b3a1573095d1684602d0660e8f
abbos <abbossetdarov3@gmail.com>
2025-10-04 19:42:52 +0500

    chats updated

--- chats/serializers.py
+++ chats/serializers.py
@@ -1,16 +1,24 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Chat, Message
 
+User = get_user_model()
+
+class UserShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "first_name", "last_name", "profile_image"]
+
 class MessageSerializer(serializers.ModelSerializer):
-    sender = serializers.StringRelatedField(read_only=True)
+    sender = UserShortSerializer(read_only=True)
 
     class Meta:
         model = Message
         fields = ["id", "chat", "sender", "text", "created_at", "is_read"]
 
-
 class ChatSerializer(serializers.ModelSerializer):
     last_message = serializers.SerializerMethodField()
+    participants = UserShortSerializer(many=True, read_only=True)  # 🔹 to‘liq user info
 
     class Meta:
         model = Chat
