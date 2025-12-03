cc529df6d60b419c381bd8757c0aed95acd74aa8
abbos <abbossetdarov3@gmail.com>
2025-12-03 21:41:37 +0500

    Fix CSRF settings for production

--- headhunter_backend/settings.py
+++ headhunter_backend/settings.py
@@ -151,8 +151,7 @@ CACHES = {
     }
 }
 
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
 
 # --------------------------------------------------
 # ✅ REST Framework
